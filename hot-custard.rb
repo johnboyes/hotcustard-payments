6d5c57e42836e8e64ded39368a1f2c5b65de5d8b
John Boyes <john@edinburghwebservices.co.uk>
2015-05-16 11:17:40 +0100

    outputting content from spreadsheet successfully

--- hot-custard.rb
+++ hot-custard.rb
@@ -3,21 +3,23 @@ require 'google/api_client'
 require 'google_drive'
 require 'pry'
 
-CLIENT_ID = ENV['CLIENT_ID']
+GOOGLE_SERVICE_ACCOUNT_EMAIL_ADDRESS = ENV['GOOGLE_SERVICE_ACCOUNT_EMAIL_ADDRESS']
 PRIVATE_KEY = ENV['PRIVATE_KEY']
+SPREADSHEET_KEY = ENV['SPREADSHEET_KEY']
 GOOGLE_API_VERSION = 'v2'
 
 get '/' do
   client = Google::APIClient.new application_name: '[App name]', application_version: '1.0'
-  key = OpenSSL::PKey::RSA.new PRIVATE_KEY, 'notasecret'
+  private_key = OpenSSL::PKey::RSA.new PRIVATE_KEY, 'notasecret'
   client.authorization = Signet::OAuth2::Client.new(
-  :token_credential_uri => 'https://accounts.google.com/o/oauth2/token',
-  :audience => 'https://accounts.google.com/o/oauth2/token',
-  :scope => 'https://www.googleapis.com/auth/drive https://spreadsheets.google.com/feeds/',
-  :issuer => CLIENT_ID,
-  :signing_key => key)
+    :token_credential_uri => 'https://accounts.google.com/o/oauth2/token',
+    :audience => 'https://accounts.google.com/o/oauth2/token',
+    :scope => 'https://www.googleapis.com/auth/drive https://spreadsheets.google.com/feeds/',
+    :issuer => GOOGLE_SERVICE_ACCOUNT_EMAIL_ADDRESS,
+    :signing_key => private_key)
   auth = client.authorization
   auth.fetch_access_token!
   session = GoogleDrive.login_with_oauth(auth.access_token)
-  "number of spreadsheets found: #{session.spreadsheets.size}"
+  worksheet = session.spreadsheet_by_key(SPREADSHEET_KEY).worksheets[0]
+  worksheet[2, 1]
 end
