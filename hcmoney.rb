c64b9aa720271bf2953bb561c0e1989c91154236
John Boyes <john@edinburghwebservices.co.uk>
2018-07-18 21:32:29 +0100

    oops

--- hcmoney.rb
+++ hcmoney.rb
@@ -9,7 +9,7 @@ I18n.enforce_available_locales = false
 class HCMoney
   Money.default_bank = MoneyOXR::Bank.new(
     app_id: ENV['OPEN_EXCHANGE_RATES_APP_ID'],
-    cache_path: 'tmp/oxr.json',
+    cache_path: '/tmp/oxr.json',
     max_age: 86_400
   )
 
