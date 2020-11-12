529b17d67215aaf74be0539ffea84f7b4e0d1363
John Boyes <john@agilepathway.co.uk>
2020-11-12 23:28:14 +0700

    Remove duplicated branch as per RuboCop

--- hcmoney.rb
+++ hcmoney.rb
@@ -24,8 +24,7 @@ class HCMoney
     return [hot_custard_balance, -creditor_balance].max if hot_custard_balance.negative?
 
     case hot_custard_balance <=> creditor_balance
-    when -1 then hot_custard_balance
-    when 0 then hot_custard_balance
+    when -1,0 then hot_custard_balance
     when 1 then creditor_balance
     end
   end
